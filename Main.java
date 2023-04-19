8e6818d73ebcb2acfc0cd8d134e758da6186e169
Dineth Jayasinghe <dmuthusara@gmail.com>
2023-04-19 19:31:18 +0530

    Scale

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Shift shift1 = new Shift("Morning Shift", "9:00", "17:00");
         Shift shift2 = new Shift("Afternoon Shift", "17:00", "01:00");
-        Shift shift3 = new Shift("Afternoon Shift", "01:00", "09:00");
+        Shift shift3 = new Shift("Midnight Shift", "01:00", "09:00");
 
         List<Shift> shifts = new ArrayList<Shift>();
         shifts.add(shift1);
