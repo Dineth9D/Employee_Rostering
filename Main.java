92721fa70f79cd08627713bd81a5db1fadcfa010
Dineth Jayasinghe <dmuthusara@gmail.com>
2023-04-19 16:44:29 +0530

    increase the shifts

--- Main.java
+++ Main.java
@@ -3,12 +3,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Shift shift1 = new Shift("Morning Shift", "9:00", "13:00");
-        Shift shift2 = new Shift("Afternoon Shift", "14:00", "18:00");
+        Shift shift1 = new Shift("Morning Shift", "9:00", "17:00");
+        Shift shift2 = new Shift("Afternoon Shift", "17:00", "01:00");
+        Shift shift3 = new Shift("Afternoon Shift", "01:00", "09:00");
 
         List<Shift> shifts = new ArrayList<Shift>();
         shifts.add(shift1);
         shifts.add(shift2);
+        shifts.add(shift3);
 
         Employee emp1 = new Employee("Alice", List.of("9:00", "10:00", "11:00"));
         Employee emp2 = new Employee("Bob", List.of("14:00", "15:00", "16:00"));
