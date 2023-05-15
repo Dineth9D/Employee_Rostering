808cf59ba9acf2869782579b430f12968de8ea29
Dineth Jayasinghe <dmuthusara@gmail.com>
2023-05-16 00:13:56 +0530

    Create as Spring Boot project

---
+++ src/test/java/org/employee_rostering/EmployeeRosterSystemTests.java
@@ -0,0 +1,14 @@
+package org.employee_rostering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EmployeeRosterSystemTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
